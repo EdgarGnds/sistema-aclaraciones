2ca9462811153e85ee81308c74ec7613ae37318f
Edgar Granados <edgargdo03@gmail.com>
2025-04-23 20:01:55 -0600

    registro de clientes funcionando

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///C:/Users/hp/Documents/sistema-aclaraciones/data/aclaraciones.db")
+SQLALCHEMY_DATABASE_URI = "mysql://root:Nata542247@localhost/aclaraciones_db"
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
